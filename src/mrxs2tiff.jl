2a46df1d86e54339fdab10b1818622b0bf33f3b8
Leonard Romano <lromano@usm.lmu.de>
2023-03-20 19:36:20 +0100

    updated support for batch shell

--- src/mrxs2tiff.jl
+++ src/mrxs2tiff.jl
@@ -12,13 +12,13 @@ module Bioformat_To_Tiff_Converter
         
         Optional Arguments:
         - dir_out::String            -- path to output directory (default: folder)
-        - bioformats2raw::String     -- command to call bioformats2raw (default: "bioformats2raw")
-        - opt_bioformats2raw::String -- runtime options for bioformats2raw conversion (default: "")
-        - raw2ometiff::String        -- command to call raw2ometiff (default: "raw2ometiff")
-        - opt_raw2ometiff::String    -- runtime options for raw2ometiff conversion (default: "")
+        - bioformats2raw::Vector{String}     -- command to call bioformats2raw (default: "bioformats2raw")
+        - opt_bioformats2raw::Vector{String} -- runtime options for bioformats2raw conversion (default: "")
+        - raw2ometiff::Vector{String}        -- command to call raw2ometiff (default: "raw2ometiff")
+        - opt_raw2ometiff::Vector{String}    -- runtime options for raw2ometiff conversion (default: "")
     """
-    function mrxs2tiff(folder::String; dir_out::String=folder, bioformats2raw::String="bioformats2raw", opt_bioformats2raw::String="", 
-                       raw2ometiff::String="raw2ometiff", opt_raw2ometiff::String="")
+    function mrxs2tiff(folder::String; dir_out::String=folder, bioformats2raw::Vector{String}=["bioformats2raw"], opt_bioformats2raw::Vector{String}=[""], 
+                       raw2ometiff::Vector{String}=["raw2ometiff"], opt_raw2ometiff::Vector{String}=[""])
         # check if output directory exists
         isdir(dir_out) ? nothing : mkpath(dir_out)
 
@@ -43,19 +43,19 @@ module Bioformat_To_Tiff_Converter
         - dir_out::String            -- path to output directory 
     
         Optional Arguments:
-        - bioformats2raw::String     -- command to call bioformats2raw (default: "bioformats2raw")
-        - opt_bioformats2raw::String -- runtime options for bioformats2raw conversion (default: "")
-        - raw2ometiff::String        -- command to call raw2ometiff (default: "raw2ometiff")
-        - opt_raw2ometiff::String    -- runtime options for raw2ometiff conversion (default: "")
+        - bioformats2raw::Vector{String}     -- command to call bioformats2raw (default: "bioformats2raw")
+        - opt_bioformats2raw::Vector{String} -- runtime options for bioformats2raw conversion (default: "")
+        - raw2ometiff::Vector{String}        -- command to call raw2ometiff (default: "raw2ometiff")
+        - opt_raw2ometiff::Vector{String}    -- runtime options for raw2ometiff conversion (default: "")
     """
-    function mrxs2tiff(path::String, fname::String, dir_out::String; bioformats2raw::String="bioformats2raw", opt_bioformats2raw::String="", 
-                       raw2ometiff::String="raw2ometiff", opt_raw2ometiff::String="")
+    function mrxs2tiff(path::String, fname::String, dir_out::String; bioformats2raw::Vector{String}=["bioformats2raw"], opt_bioformats2raw::Vector{String}=[""], 
+                       raw2ometiff::Vector{String}=["raw2ometiff"], opt_raw2ometiff::Vector{String}=[""])
         # convert mrxs to pyramid file
         run_windows(`$(bioformats2raw) $(opt_bioformats2raw) $(path) $(dir_out)/zarr-pyramid`)
         # convert pyramid to tiff
         run_windows(`$(raw2ometiff) $(opt_raw2ometiff) $(dir_out)/zarr-pyramid $(dir_out)/$(fname).ome.tiff`)
         # remove temporary files
-        run_windows(`rm -r $(dir_out)/zarr-pyramid`)
+        delete_windows("$(dir_out)/zarr-pyramid")
     end
 
     "Check whether or not a path corresponds to an .mrxs file"
@@ -69,6 +69,15 @@ module Bioformat_To_Tiff_Converter
             run(command)
         catch e
              run(`$shell /c $command`)
+        end
+    end
+
+    "Delete path in bash or batch shell"
+    function delete_windows(path::String)
+        try
+            run(`rm -r $(path)`)
+        catch e
+            run(`cmd /c rmdir /s /q $(path)`)
         end
     end
 end
